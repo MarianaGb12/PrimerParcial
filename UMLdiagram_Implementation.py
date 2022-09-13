17742dc5ed076cb7be60b3c80577411e7acabc82
MarianaGb12 <98919850+MarianaGb12@users.noreply.github.com>
2022-09-13 09:08:03 -0500

    Create UMLdiagram_Implementation.py

---
+++ UMLdiagram_Implementation.py
@@ -0,0 +1,24 @@
+class User:
+    def _init_(self, name: str, id:int) -> None:
+        self.name = name
+        self.id = id
+    
+    def borrow_book(self, name_of_book:str):
+        pass
+    
+    def return_book(self, name_of_book: str):
+        pass
+    
+    def display_available_Books(self):
+        pass
+
+class Librarian:
+    def _init_(self, name: str, list_books: list) -> None:
+        self.name = name
+        self.list_books = list_books
+    
+    def upload_list_of_books(self, name_of_book: str):
+        pass
+
+    def modify_list(self, name_of_book: str):
+        pass
